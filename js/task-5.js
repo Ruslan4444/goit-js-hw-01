67bfc19f08f1e360c7e77c079cc0d2043c949741
Ruslan4444 <ruslankv4@gmail.com>
2020-12-05 23:28:30 +0100

    Виконано завдання 5

---
+++ js/task-5.js
@@ -0,0 +1,39 @@
+
+let cost;
+let myCountry = prompt('Введите страну для доставки');
+
+if (myCountry !== null) {
+    myCountry = myCountry.toLowerCase();
+} 
+
+
+
+switch (myCountry) {
+    case 'китай':
+        cost = 100;
+        console.log(`Доставка в ${myCountry} будет стоить ${cost} кредитов`);
+        break;
+    case 'чили':
+        cost = 250;
+        console.log(`Доставка в ${myCountry} будет стоить ${cost} кредитов`);
+        break;
+    case 'австралия':
+        cost = 170;
+        console.log(`Доставка в ${myCountry} будет стоить ${cost} кредитов`);
+        break;
+    case 'индия':
+        cost = 80;
+        console.log(`Доставка в ${myCountry} будет стоить ${cost} кредитов`);
+        break;
+    case 'ямайка':
+        cost = 120;
+        console.log(`Доставка в ${myCountry} будет стоить ${cost} кредитов`);
+        break;
+    default:
+        alert('В вашей стране доставка не доступна');
+        
+   
+}
+
+    
+
