940484de37ee9722e49f3a5c0662d5cd76857ea1
Ruslan4444 <ruslankv4@gmail.com>
2020-12-05 23:59:16 +0100

    Виконано завдання 6

---
+++ js/task-6.js
@@ -0,0 +1,12 @@
+let input;
+let total = 0;
+
+while (true) {
+    input = prompt('Введите число');
+
+    if (input === null) {
+        alert(`Общая сумма чисел равна ${total}`);
+        break;
+    }
+    total += Number(input)
+}
